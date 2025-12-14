5bbcc439f73764ef96977e5748552aa754d949ee
Fitri Nurhidayat <fitrinurhidayat42@gmail.com>
2025-12-14 23:05:35 +0700

    Route Room

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
+use App\Http\Controllers\RoomController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('hotels', HotelController::class);
-
-
+Route::resource('rooms', RoomController::class);
