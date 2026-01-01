4e525a3aa1b622e40a8d49978df6d2c62667db0e
Nabila Khairunnisa <bilaaa.smiles@gmail.com>
2026-01-01 15:52:01 +0700

    i

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,12 +39,12 @@
                         <a href="{{ route('dashboard') }}" class="nav-link">Dashboard</a>
                         <a href="{{ route('hotels.index') }}" class="nav-link">Hotels</a>
                         <a href="{{ route('rooms.index') }}" class="nav-link">Rooms</a>
+                        <a href="{{ route('bookings.history') }}" class="nav-link">Bookings</a>
                     @endif
 
                     @if(auth()->user()->role === 'super_admin')
                         <a href="{{ route('dashboard') }}" class="nav-link">Dashboard</a>
                         <a href="{{ route('hotels.index') }}" class="nav-link">Hotels</a>
-                        <a href="{{ route('bookings.history') }}" class="nav-link">Bookings</a>
                         <a href="#" class="nav-link">Users</a>
                     @endif
                 @endauth
